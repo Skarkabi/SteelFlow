c997fc4897e4419bced761e71d5ced5f1daa22ae
saleem karkabi <saleemkarkabi@cmail.carleton.ca>
2021-10-09 19:54:17 +0400

    User routes code outline

---
+++ server/routes/user.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import User from '../models/User';
+
+const router = express.Router();
+
+router.get('/view', (req,res,next) => {
+
+});
+
+router.get('/view/:id', (req,res,next) => {
+
+});
+
+router.get('/view/staff/:id', (req, res, next) => {
+
+});
+
+router.post('/update/:id', (req,res,next) => {
+
+});
+
+router.post('/delete/:id', (req,res,next) => {
+
+});
+
+export default router;
