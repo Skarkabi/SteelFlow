b147616745acec879b47725c3c2f354d611e5593
saleem karkabi <saleemkarkabi@cmail.carleton.ca>
2021-10-09 20:04:36 +0400

    Added Comments

--- server/routes/order.js
+++ server/routes/order.js
@@ -2,34 +2,42 @@ import express from 'express';
 
 const router = express.Router();
 
+//Express Route to view all orders
 router.get('/view', (req,res,next) => {
 
 });
 
+//Express Route to view specefic order
 router.get('/view/:id', (req,res,next) => {
 
 });
 
+//Express Route to create order request
 router.post('/request', (req,res,next) => {
 
 });
 
+//Exporess Route to delete order
 router.post('/delete/:id', (req,res,next) => {
 
 });
 
+//Express Route to update order
 router.post('/update/:id', (req,res,next) => {
 
 });
 
+//Express Route to approve/reject order
 router.post('/approved/:id/:approved', (req,res,next) => {
 
 });
 
+//Express Route to update order item status
 router.post('/item/update/:id/:item_id', (req,res,next) => {
 
 });
 
+//Express Route to create material request
 router.post('/material_request/request/:id/:mr_id', (req,res,next) => {
 
 });
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,22 +2,27 @@ import express from 'express';
 
 const router = express.Router();
 
+//Express Route to view all users
 router.get('/view', (req,res,next) => {
 
 });
 
+//Express Route to view specific user
 router.get('/view/:id', (req,res,next) => {
 
 });
 
+//Express Route to view all employees of manager
 router.get('/view/staff/:id', (req, res, next) => {
 
 });
 
+//Express Route to update user
 router.post('/update/:id', (req,res,next) => {
 
 });
 
+//Expres route to delete user
 router.post('/delete/:id', (req,res,next) => {
 
 });
